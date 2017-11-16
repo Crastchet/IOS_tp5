16b57ed26656ab94a16ba1f632a9443a21e85342
Thibs <crastchet@gmail.com>
2017-11-16 12:49:55 +0100

    Began to write a few tests. Next time use JUnit for managing excpetions

--- cxf-bank-client/src/main/java/ios/bank/client/Main.java
+++ cxf-bank-client/src/main/java/ios/bank/client/Main.java
@@ -6,6 +6,7 @@ import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
 import javax.xml.datatype.XMLGregorianCalendar;
 
+import ios.bank.generated.Customer;
 import ios.bank.generated.CustomerAlreadyExistException_Exception;
 import ios.bank.generated.CustomerNoExistException_Exception;
 import ios.bank.generated.WebServiceBankInterface;
@@ -17,16 +18,41 @@ public class Main {
 		GregorianCalendar c = new GregorianCalendar(1995,05,13);
 		XMLGregorianCalendar birth =  DatatypeFactory.newInstance().newXMLGregorianCalendar(c);
 		
+		GregorianCalendar c2 = new GregorianCalendar(1997,04,17);
+		XMLGregorianCalendar birth2 =  DatatypeFactory.newInstance().newXMLGregorianCalendar(c);
+		
 		BankServiceClient client = new BankServiceClient();
 		WebServiceBankInterface port = client.getService();
 		
+		
+		/** Methods : 
+		 * 			createCustomer(String firstname, String lastname, Calendar birth)
+		 * 			getCustomer(String firstname, String lastname, Calendar birth)
+		 */
+		//Récupération de clients qui existent
 		try {
-			//port.createCustomer("Thibault", "Coilliaux", birth);
-			System.out.println(port.getCustomer("Thibault", "Coilliaux", birth));
-		} catch (CustomerNoExistException_Exception e1) {
+			//On crée 1 client, puis on le récupère
+			Customer customer1 = port.createCustomer("Thibault", "Coilliaux", birth);
+			Customer customerGot1 = port.getCustomer("Thibault", "Coilliaux", birth);
+			assertEquals(customer1,customerGot1);
+			
+			Customer customer2 = port.createCustomer("Antoine", "Coilliaux", birth2);
+			Customer customerGot2 = port.getCustomer("Antoine", "Coilliaux", birth2);
+			assertEquals(customer2,customerGot2);
+			
+			assertNotEquals(customerGot1, customerGot2);
+			
+		} catch (CustomerAlreadyExistException_Exception e) {
 			// TODO Auto-generated catch block
-			e1.printStackTrace();
+			e.printStackTrace();
+			return;
+		} catch (CustomerNoExistException_Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return;
 		}
+		
+		
 		
 	}
 }
